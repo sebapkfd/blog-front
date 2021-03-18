4338657daa3b5d9b367737c8d3a89b3a08b389bb
Sebastian Soto <sebastian.pkfd@gmail.com>
2021-03-18 19:25:26 -0300

    Delete post funtionality added

--- src/components/postItem.js
+++ src/components/postItem.js
@@ -3,8 +3,25 @@ import React from 'react';
 const PostItem = (props) => {
     const {post} = props;
 
+    const deletePost = async (e) => {
+        e.preventDefault();
+        try {
+            await fetch('http://localhost:5000/api/posts/:' + post._id, {
+                method: 'DELETE',
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": 'Bearer ' + JSON.parse(localStorage.getItem('userSession')).token
+                },
+                body: JSON.stringify({id: post._id})
+            })
+            window.location.reload();
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
     const userId = (localStorage.length > 0) ? JSON.parse(localStorage.getItem('userSession')).user._id : null;
-    const deleteButton = (post.user._id === userId) ? <button>Delete</button> : null;
+    const deleteButton = (post.user._id === userId) ? <button onClick={deletePost}>Delete</button> : null;
     const editButton = (post.user._id === userId) ? <button>Edit</button> : null;
 
     return(
