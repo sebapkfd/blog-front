b0f37287ed493ab8530871111d746d2408ebab6c
Sebastian Soto <sebastian.pkfd@gmail.com>
2021-03-18 18:09:03 -0300

    Index displays only published posts

--- src/components/post.js
+++ src/components/post.js
@@ -5,12 +5,12 @@ const Post = () => {
     const [title, setTitle] = useState('');
     const [text, setText] = useState('');
 
-    const submitData = async (e) => {
+    const submitData = async (e, isPublished) => {
         e.preventDefault();
         try {
             const user = JSON.parse(localStorage.getItem('userSession')).user._id;
             const timestamp = new Date().toLocaleString();
-            const body = {title, text, user, timestamp, published: true}
+            const body = {title, text, user, timestamp, published: isPublished}
             const response = await fetch('http://localhost:5000/api/posts', {
                 method: 'POST',
                 headers: {
@@ -28,7 +28,7 @@ const Post = () => {
     return (
         <div>
             <h1>Publish a Post!</h1>
-            <form onSubmit={submitData}>
+            <form >
                 <label>Title</label>
                 <input
                     type='text'
@@ -45,7 +45,8 @@ const Post = () => {
                     value={text}
                     onChange={e => setText(e.target.value)}
                 />
-                <button>Post</button>
+                <button onClick={(e) =>submitData(e, false)}>Save as Unpublished</button>
+                <button onClick={(e)=> submitData(e, true)}>Publish post</button>
             </form>
         </div>
     )
